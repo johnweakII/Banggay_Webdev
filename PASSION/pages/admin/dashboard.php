6b26cb64fd301f495907ba8987924864c97abd82
johnweakII <johnraybanggay@gmail.com>
2024-05-12 12:15:17 -0700

    v1.1
    Project2

---
+++ PASSION/pages/admin/dashboard.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.5">
+    <title>Document</title>
+</head>
+<body style="overflow: hidden;">
+    <canvas style="background-color: rgba(248, 148, 148, 0.699);
+    width: 500px;
+    height: 500px;
+    text-align: center;
+    position: absolute;
+    inset-inline-start: 30%;
+   
+   "></canvas>
+    <canvas style="background-color: rgba(131, 255, 142, 0.308) ;
+    text-align: center;
+    position: absolute;
+    inset-block-start: 25%;
+    inset-inline-start:40%;
+    width: 500px;
+    height: 500px;
+  "></canvas>
+ 
+</body>
+</html>
